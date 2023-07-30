ab17d1cdef0f83e34fc585759e215a0edb2dd8f1
Sumandari <sumandari.ee@gmail.com>
2023-07-30 09:48:21 +0200

    routing parameter

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { products } from '../data/products';
 
 export function ProductsPage() {
@@ -6,8 +7,10 @@ export function ProductsPage() {
       <h2 className="text-x1 font-bold text-slate-600">Here are some great tools for react</h2>
       <ul className="list-none m-0 p-0">
         {products.map((product) => (
-          <li key={product.id} className="p-1 text-base text-slate-800">
-            {product.name}
+          <li key={product.id}>
+            <Link to={`${product.id}`} className="p-1 text-base text-slate-800 hover:underline">
+              {product.name}
+            </Link>
           </li>
         ))}
       </ul>
