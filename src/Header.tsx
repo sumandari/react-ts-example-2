f883131ba886e3b79db23ea69d7623cac9b6c8c2
Sumandari <sumandari.ee@gmail.com>
2023-07-31 10:54:00 +0200

    use Form; remove navigate

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,19 +1,12 @@
 import { FormEvent } from 'react';
-import { NavLink, Link, useSearchParams, useNavigate } from 'react-router-dom';
+import { NavLink, Link, useSearchParams, Form } from 'react-router-dom';
 import logo from './logo.svg';
 
 export function Header() {
   const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
-  function handleSearchSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const search = formData.get('search') as string;
-    navigate(`/products/?search=${search}`);
-  }
   return (
     <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
-      <form className="relative text-right" onSubmit={handleSearchSubmit}>
+      <Form className="relative text-right" action="/products">
         <input
           type="search"
           name="search"
@@ -21,7 +14,7 @@ export function Header() {
           defaultValue={searchParams.get('search') ?? ''}
           className="absolute right-0 top-0 rounded py-2 px-3 text-gray-700"
         />
-      </form>
+      </Form>
       <Link to="">
         <img src={logo} alt="Logo" className="inline-block h-20" />
       </Link>
