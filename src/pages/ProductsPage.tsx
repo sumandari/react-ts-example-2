4af37713eb601274cbea6a472f052d20b152ccc4
Sumandari <sumandari.ee@gmail.com>
2023-07-31 10:07:11 +0200

    filter product

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { products } from '../data/products';
 
 export function ProductsPage() {
+  const [searchParams] = useSearchParams();
+  function getFilteredProducts() {
+    const search = searchParams.get('search');
+    if (search === null || search === '') {
+      return products;
+    } else {
+      return products.filter(
+        (product) => product.name.toLocaleLowerCase().indexOf(search.toLowerCase()) > -1,
+      );
+    }
+  }
   return (
     <div className="text-center p-5">
       <h2 className="text-x1 font-bold text-slate-600">Here are some great tools for react</h2>
       <ul className="list-none m-0 p-0">
-        {products.map((product) => (
+        {getFilteredProducts().map((product) => (
           <li key={product.id}>
             <Link to={`${product.id}`} className="p-1 text-base text-slate-800 hover:underline">
               {product.name}
